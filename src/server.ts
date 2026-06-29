66c61811ecfdcb8f10581afb67b7dfbd7ddd5bc8
Daredre31 <isaiahola58@gmail.com>
2026-06-29 16:07:53 -0400

    new

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173" , "https://hostelos-ashy.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
      exposedHeaders: [
